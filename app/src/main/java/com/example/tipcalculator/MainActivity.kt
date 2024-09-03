99f8ac6c9dbe46845988225a49415ba5a9a72c0d
Muhammad Murtaza Hussain <murtazah1218@gmail.com>
2024-09-03 23:13:26 +0500

    made the tip amount functional and displayed it, also used state hoisting.

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -24,11 +25,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tipcalculator.ui.theme.TipCalculatorTheme
 import java.text.NumberFormat
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.input.KeyboardType
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,6 +58,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun TipCalculatorLayout(){
+    
+    // used hoisting here to make the edit text fun stateless and this fun stateful
+    //state is a variable or data that can change over time and which the compose closely
+    // monitors
+
+    var amountInput by remember { mutableStateOf("") }
+    // ?: is the elvis operator that returns the before expression when the value is not null
+    //and returns the expression after if the value is null.
+    val amount = amountInput.toDoubleOrNull()?:0.0
+    val tip = calculateTip(amount)
     Column(
         modifier = Modifier
             .statusBarsPadding()
@@ -71,11 +82,13 @@ fun TipCalculatorLayout(){
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
-        EditNumberField(modifier = Modifier
+        EditNumberField(value = amountInput,
+            onValueChange = {amountInput=it},
+            modifier = Modifier
             .padding(bottom = 32.dp)
             .fillMaxWidth())
         Text(
-            text = stringResource(id = R.string.tip_amount, "Rs0.00"),
+            text = stringResource(id = R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
         )
         Spacer(modifier = Modifier.height(150.dp))
@@ -104,10 +117,19 @@ typed so far!
 
  */
 @Composable
-fun EditNumberField(modifier: Modifier=Modifier){
-    var amount by remember { mutableStateOf("") }
-    TextField(value = amount,
-        onValueChange ={amount=it},
+fun EditNumberField(
+    value: String,
+    //accepts a string value and returns nothing
+    onValueChange: (String)->Unit,
+    modifier: Modifier=Modifier){
+
+    TextField(value = value,
+        onValueChange =onValueChange,
+        label = { Text(text = stringResource(id = R.string.bill_amount))},
+        //keyboard options lets us specify what kind of keyboard to display when clicked on textfield
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        //single line property wraps the text into a horizontally scrollable single line.
+        singleLine = true,
         modifier=modifier
         )
 }
@@ -116,7 +138,9 @@ fun EditNumberField(modifier: Modifier=Modifier){
 * the value automaticLLY to 15. */
 private fun calculateTip(amount: Double, tipPercent:Double = 15.0):String{
     val tip = tipPercent / 100 * amount
-    return NumberFormat.getCurrencyInstance().format(tip)//this line converts the currency into the format according to location o the device and returns as a string
+    return NumberFormat.getCurrencyInstance().format(tip)
+//this line converts the currency into the format according to location of
+// the device and returns as a string
 }
 @Preview(showBackground = true)
 @Composable
